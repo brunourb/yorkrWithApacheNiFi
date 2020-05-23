aa601bf0a5aa3679e1ee08b79ed2315b750ae590
Tinniam V Ganesh <tvganesh@Apples-MacBook-Pro.local>
2020-05-23 09:29:25 +0530

    Apache NiFi with yorkr

---
+++ convertYaml2DataFrame.R
@@ -0,0 +1,3 @@
+library(yorkr)
+args<-commandArgs(TRUE)
+convertYaml2RDataframeT20(args[1], args[2], args[3])
---
+++ rankIPL.R
@@ -0,0 +1,4 @@
+library(yorkr)
+args<-commandArgs(TRUE)
+
+rankIPLBatsmen(args[1],args[2],args[3])
